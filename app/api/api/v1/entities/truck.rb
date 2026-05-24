316f8f903306cfd31d27e9f81b2103a3c123ec30
alimourana <alimourana@gmail.com>
2026-05-23 23:19:27 -0400

    Update API entity documentation types for consistency
    - Changed documentation types from "string" and "integer" to "String" and "Integer" for consistency across the Session, Truck, and User entities.
    - Updated date fields in the Truck entity to use "DateTime" for better clarity in documentation.

--- app/api/api/v1/entities/truck.rb
+++ app/api/api/v1/entities/truck.rb
@@ -2,16 +2,16 @@
 
 module API::V1::Entities
   class Truck < Base
-    expose :id, documentation: {type: "integer", desc: "The ID of the truck."}
-    expose :plate_number, documentation: {type: "string", desc: "The plate number of the truck."}
-    expose :vin, documentation: {type: "string", desc: "The VIN of the truck."}
-    expose :make, documentation: {type: "string", desc: "The make of the truck."}
-    expose :model, documentation: {type: "string", desc: "The model of the truck."}
-    expose :year, documentation: {type: "integer", desc: "The year of the truck."}
-    expose :status, documentation: {type: "string", desc: "The status of the truck."}
-    expose :created_by_id, documentation: {type: "integer", desc: "The ID of the user who created the truck."}
-    expose :created_at, format_with: :iso_8601_date, documentation: {type: "string", desc: "The creation date (YYYY-MM-DD)."}
-    expose :updated_at, format_with: :iso_8601_date, documentation: {type: "string", desc: "The last update date (YYYY-MM-DD)."}
-    expose :discarded_at, format_with: :iso_8601_date, documentation: {type: "string", desc: "The discard date (YYYY-MM-DD)."}
+    expose :id, documentation: {type: "Integer", desc: "The ID of the truck."}
+    expose :plate_number, documentation: {type: "String", desc: "The plate number of the truck."}
+    expose :vin, documentation: {type: "String", desc: "The VIN of the truck."}
+    expose :make, documentation: {type: "String", desc: "The make of the truck."}
+    expose :model, documentation: {type: "String", desc: "The model of the truck."}
+    expose :year, documentation: {type: "Integer", desc: "The year of the truck."}
+    expose :status, documentation: {type: "String", desc: "The status of the truck."}
+    expose :created_by_id, documentation: {type: "Integer", desc: "The ID of the user who created the truck."}
+    expose :created_at, documentation: {type: "DateTime", desc: "The creation time."}
+    expose :updated_at, documentation: {type: "DateTime", desc: "The last update time."}
+    expose :discarded_at, documentation: {type: "DateTime", desc: "The discard time."}
   end
 end
