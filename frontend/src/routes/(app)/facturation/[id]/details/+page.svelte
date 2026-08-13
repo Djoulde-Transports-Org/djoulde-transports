284c35842e94b305e9ca3cf7cff6e587d886614c
alimourana <alimourana@gmail.com>
2026-08-13 14:19:46 -0400

    Add details page for billing statements
    - Created a new Svelte page to display detailed billing statement information using the BillingStatementDetail component.
    - Integrated routing to allow access to billing details based on the statement ID from the URL parameters.

---
+++ frontend/src/routes/(app)/facturation/[id]/details/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type {PageProps} from './$types';
+  import BillingStatementDetail from '$lib/components/facturation/BillingStatementDetail.svelte';
+
+  let {params}: PageProps = $props();
+</script>
+
+<div class="p-8">
+  <BillingStatementDetail id={params.id} />
+</div>
